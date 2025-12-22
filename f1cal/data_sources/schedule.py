29e4b97d413bb4633b16005f22f84374168af014
Luke Moll <luke.moll99@gmail.com>
2025-12-22 14:26:20 +0000

    Add F1 fonts, text test, and basic countdown.
     * Commit F1 style OTF fonts
     * Add Text endpoint showcasing multiline_text and anchors
     * Add Countdown endpoint -- currently bypassing Calendar API before caching is available.

--- f1cal/data_sources/schedule.py
+++ f1cal/data_sources/schedule.py
@@ -11,6 +11,8 @@ __all__ = ["get_next_grand_prix", "get_all_upcoming_grands_prix"]
 # This URL doesn't include Sprint races; will need to update to handle that.
 URL = "https://files-f1.motorsportcalendars.com/f1-calendar_gp.ics"
 
+# TODO: add cached ICS from filesystem.
+
 def fetch_ics() -> icalendar.Calendar:
     # Fetch the URL
     res = requests.get(URL)
@@ -20,13 +22,17 @@ def fetch_ics() -> icalendar.Calendar:
     # otherwise throw an exception so the caller can say "data unavailable" or something
 
     ics = icalendar.Calendar.from_ical(res.content)
-    
+
     return ics
 
-def get_next_grand_prix():
-    ics = fetch_ics()
+def get_next_grand_prix() -> icalendar.Event:
+    ics = fetch_ics() # TODO: use cached ICS or pass by parameter.
 
-    
+    # TODO: this isn't correct; need to filter only events in the future(ish)
+    all_events = sorted(ics.events, key=lambda ev:ev.DTSTART)
+    nextgp = next(iter(all_events))
+
+    return nextgp
 
     # Find the next Event that isn't in the past
     # Depending on how convenient the `icalendar` object is to work with, we could define a new object
--- f1cal/routes.py
+++ f1cal/routes.py
@@ -1,7 +1,11 @@
-from .pillow_helpers import serve_image_inky, EPaperDisplay, InkyCol
+from .pillow_helpers import serve_image_inky, EPaperDisplay, InkyCol, break_lines
+from .font import F1Reg, F1Bold, F1Wide
+from .data_sources import schedule
 
 from bottle import route, run, request, abort
 from PIL import ImageDraw
+from datetime import datetime as dt, timezone as tz
+
 
 @route('/')
 def index():
@@ -13,14 +17,100 @@ def hello_inky(draw : ImageDraw, epd : EPaperDisplay):
     """
         "Test pattern" colour bars
     """
-    TOTAL_WIDTH = 800
-    TOTAL_HEIGHT = 480
     NUM_COLS = len(InkyCol)
     print(f"{NUM_COLS=}")
-    COL_WIDTH = TOTAL_WIDTH / NUM_COLS
+    COL_WIDTH = epd.WIDTH / NUM_COLS
 
     for i,colour in enumerate(InkyCol):
         start_xy = (i*COL_WIDTH, 0)
-        end_xy = ((i+1)*COL_WIDTH, 480)
+        end_xy = ((i+1)*COL_WIDTH, epd.HEIGHT)
         print(i, colour, start_xy, end_xy)
         draw.rectangle([start_xy, end_xy], fill=colour.value, width=0)
+
+@route('/inky/text')
+@serve_image_inky
+def text_inky(draw : ImageDraw, epd : EPaperDisplay):
+    draw.rectangle([0,0,epd.WIDTH,epd.HEIGHT], fill=InkyCol.RED.value)
+
+    draw.text([5, 5], text="2022 British Grand Prix", font=F1Wide(21), fill=InkyCol.BLACK.value)
+
+    margin = epd.WIDTH * 0.05
+
+    active_width = epd.WIDTH - margin * 2
+
+    script1 = "\"Leclerc has that inside line. Perez goes off the track, cuts the chicane. Off goes Leclerc...\""
+    script2 = "Through goes Hamilton, unbelievable stuff!"
+
+    script1_y = margin
+
+    font = F1Reg(36)
+    lines = list(break_lines(script1, draw, font, active_width))
+    draw.multiline_text([epd.WIDTH/2, script1_y], anchor="ma", text='\n'.join(lines), fill=InkyCol.BLACK.value, font=font)
+    bbox1 = draw.multiline_textbbox([epd.WIDTH/2, script1_y], anchor="ma", text='\n'.join(lines), font=font)
+
+    script2 = script2.upper()
+    font=F1Bold(56)
+    lines = list(break_lines(script2, draw, font, active_width))
+    draw.multiline_text([
+        bbox1[0],
+        bbox1[3] + margin/2
+        ],
+        anchor="la",
+        text='\n'.join(lines), fill=InkyCol.WHITE.value, font=font)
+
+    script3 = "123456790"
+    pt = epd.WIDTH / len(script3)
+
+    while True:
+        lines = list(break_lines(script3, draw, F1Bold(pt), epd.WIDTH))
+        if len(lines) > 1:
+            pt -= 2
+        else:
+            break
+    draw.multiline_text([epd.WIDTH, epd.HEIGHT], anchor="rd", text=script3, fill=InkyCol.ORANGE.value, font=F1Bold(pt))
+
+@route('/inky/countdown')
+@serve_image_inky
+def countdown_inky(draw : ImageDraw, epd : EPaperDisplay):
+    # next_gp = schedule.get_next_grand_prix()
+    # days : int = (next_gp.DTSTART - dt.now(tz.utc)).days
+    print("Warning: hard-coded days=75")
+    days = 75
+
+    draw.rectangle([0,0, epd.WIDTH, epd.WIDTH], fill=InkyCol.BLACK.value)
+
+    font = F1Bold(300)
+
+    # Draw number in center of screen
+    draw.text(
+        [
+            epd.WIDTH/2,
+            epd.HEIGHT/2
+        ],
+        anchor="mm",
+        text=str(days),
+        font=font,
+        fill=InkyCol.WHITE.value
+    )
+
+    bb = draw.textbbox(
+        [
+            epd.WIDTH/2,
+            epd.HEIGHT/2
+        ],
+        anchor="mm",
+        text=str(days), font=font)
+
+    caption = "days to go"
+    draw.text(
+        [
+            epd.WIDTH/2,
+            bb[3] + 8
+        ],
+        anchor="ma",
+        text=caption,
+        font=F1Reg(52),
+        fill=InkyCol.RED.value
+    )
+    
+    draw.text([0,0], anchor="la", text="2026 Formula 1 World Champtionship", font=F1Wide(18), fill=InkyCol.WHITE.value)
